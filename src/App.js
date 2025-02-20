80a577bf5de8a1c5caf1183440c36b071db0c4c4
thilakvikram <thilakvikram13@gmail.com>
2025-02-20 16:24:02 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
+import HomePage from "./page/home-page";
 
 function App() {
   return (
-    <div className="app">
-      <h1>Hello World!</h1>
-    </div>
+    <HomePage/>
   );
 }
 
